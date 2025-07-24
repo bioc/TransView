b52656635ce077666c2188b8d5d6fc5959ddd3dd
lshep <lori.shepherd@roswellpark.org>
2025-07-24 08:36:41 -0400

    Deprecated
     - unresponsive

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,9 @@
 .onAttach <- function(libname, pkgname) {
 	
 	packageStartupMessage("Welcome to TransView version ", packageDescription("TransView", fields="Version"))
-	
-}
 
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.23")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+}
